d65bacd09dd9d8ff24c86cd1f58271c975d2a50b
Nikolay Petrov <nikolay.petrov@abv.bg>
2016-01-07 08:56:08 +0200

    Server: Added new data models in AndiQuizDbContext

--- AndiQuiz.Server/Data/AndiQuiz.Server.Data/AndiQuizDbContext.cs
+++ AndiQuiz.Server/Data/AndiQuiz.Server.Data/AndiQuizDbContext.cs
@@ -23,5 +23,13 @@
         public IDbSet<Answer> Answers { get; set; }
 
         public IDbSet<Quiz> Quizs { get; set; }
+
+        public IDbSet<QuizRating> QuizRatings { get; set; }
+
+        public IDbSet<UserQuizStatistic> UserQuizStatistics { get; set; }
+
+        public IDbSet<UserScore> UserScores { get; set; }
+
+        public IDbSet<Category> Categorys { get; set; }
     }
 }
