482d086b99d5b1a354aed4536d647b72c69b09a6
Nikolay Petrov <nikolay.petrov@abv.bg>
2016-01-17 07:42:48 +0200

    Using Camera for users' profile image

--- AndiQuiz.Client/AndiQuiz.Client.Android/app/src/main/java/com/magmaelemental/andiquiz/ProfileActivity.java
+++ AndiQuiz.Client/AndiQuiz.Client.Android/app/src/main/java/com/magmaelemental/andiquiz/ProfileActivity.java
@@ -1,16 +1,24 @@
 package com.magmaelemental.andiquiz;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    int myRequestCode = 1234;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageView = (ImageView) this.findViewById(R.id.photo_taken);
     }
 
     public void moveToFindQuiz(View view) {
@@ -23,8 +31,22 @@ public class ProfileActivity extends AppCompatActivity {
         startActivity(moveNext);
     }
 
-
     public void updateImage(View view) {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePhotoIntent, myRequestCode);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == myRequestCode && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            imageView = (ImageView) this.findViewById(R.id.photo_taken);
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
