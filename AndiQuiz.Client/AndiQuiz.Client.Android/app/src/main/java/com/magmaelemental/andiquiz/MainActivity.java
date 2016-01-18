e17099bcc77818aaeb690d7a823a999b9ec3fc36
Ognyan Kossov <ogi_2112@yahoo.com>
2016-01-18 02:26:14 +0200

    added home screen

--- AndiQuiz.Client/AndiQuiz.Client.Android/app/src/main/java/com/magmaelemental/andiquiz/MainActivity.java
+++ AndiQuiz.Client/AndiQuiz.Client.Android/app/src/main/java/com/magmaelemental/andiquiz/MainActivity.java
@@ -2,15 +2,13 @@ package com.magmaelemental.andiquiz;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.magmaelemental.andiquiz.data.local.QuizDbAdapter;
 import com.magmaelemental.andiquiz.data.local.UserInfo;
